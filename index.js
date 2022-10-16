5022af8bcc9786bf9d350b55abd52a83577f2b00
Aagam Jain <aagam.jain@crio-users.in>
2022-10-16 12:12:31 +0530

    Changed all function to new arrow functions

--- index.js
+++ index.js
@@ -8,26 +8,25 @@ let total =0
 let child = 0
 let adult = 0
 
-function increment() {
+increment = () => {
     count += 1
     total+=1
     countEl.innerText = count
     totalEl.innerText = total
 }
 
-function save() {
+save = () => {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent=0
     count =0
 }
-function childincrement()
-{
+childincrement = () => {
     child += 1
     childEl.innerText = child
 }
-function adultincrement()
-{
+
+adultincrement = () =>{
     adult+= 1
     adultEl.innerText = adult
 }
